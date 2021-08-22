cf6b8a50e2d0ddc3dd99a2f49eec6b9c9344deea
geekysrm <soumyacool2012@gmail.com>
2021-08-22 06:38:07 +0530

    check if input url is hashnode blog post url or not

--- pages/api/scrape.js
+++ pages/api/scrape.js
@@ -2,12 +2,11 @@ import cheerio from "cheerio";
 import axios from "axios";
 
 export default async function handler(req, res) {
-  // const body = JSON.parse(req.body); // body sent from frontend POST request
-  const $ = await fetchHTML("https://townhall.hashnode.com/auth0-hackathon"); // send URL from backend
+  const url = req.query.url;
+  const $ = await fetchHTML(`${url}`); // send URL from backend
   // Print the full HTML
   //   console.log(`Site HTML: ${$.html()}\n\n`);
 
-  // Print some specific page content
   const firstInputId = $("input").attr("id");
   const isHashnodeBlogPostUrl = firstInputId === "hn-user"; // differentiate between blog pages and other pages? if it contains hashnode.com then it is a normal hashnode page, not a hashnode blog post
   res.status(200).json({ isHashnodeBlogPostUrl });
--- pages/enter-url.js
+++ pages/enter-url.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import axios from "axios";
+import { Input, Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
 export default function ProtectedEditPage() {
+  const [inputUrl, setInputUrl] = useState("");
+
+  const handleFetchClick = async () => {
+    const { data } = await axios.get(`/api/scrape?url=${inputUrl}`);
+    const { isHashnodeBlogPostUrl } = data;
+    console.log(isHashnodeBlogPostUrl);
+  };
+
   return (
     <div>
       <h1>Enter Hashnode Blog URL</h1>
+      <Input
+        placeholder="https://example.com"
+        value={inputUrl}
+        onChange={(e) => setInputUrl(e.target.value)}
+      />
+      <Button
+        type="primary"
+        icon={<SearchOutlined />}
+        onClick={handleFetchClick}
+      >
+        Fetch blog post
+      </Button>
     </div>
   );
 }
