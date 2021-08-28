b58db9a7427d69b4098292bd1678f23bb9850d67
geekysrm <soumyacool2012@gmail.com>
2021-08-28 06:11:01 +0530

    make more style changes in done page

--- pages/done.js
+++ pages/done.js
@@ -7,9 +7,7 @@ import Header from "../components/Header";
 export default function ProtectedDonePage({ user }) {
   const { query } = useRouter();
   const { postedTweets: postedTweetsString } = query;
-  const postedTweets = JSON.parse(postedTweetsString);
-  console.log("new", postedTweets);
-  if (!postedTweets || postedTweets.length === 0) {
+  if (!postedTweetsString) {
     return (
       <div>
         Please go to{" "}
@@ -20,6 +18,9 @@ export default function ProtectedDonePage({ user }) {
       </div>
     );
   }
+  const postedTweets = JSON.parse(postedTweetsString);
+  console.log("new", postedTweets);
+
   return (
     <div>
       <Head>
@@ -30,27 +31,32 @@ export default function ProtectedDonePage({ user }) {
         ></script>
       </Head>
       <Header user={user} />
-      <h1>Congrats! 🎉 Your tweet was successfully posted!</h1>
-      <span>
-        Click{" "}
-        <a
-          href={`https://twitter.com/${user.nickname}/status/${postedTweets[0].id_str}`}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          here
-        </a>{" "}
-        to view it on Twitter.
-      </span>
-      <div>
-        {postedTweets.map((tweet) => (
-          <blockquote class="twitter-tweet">
-            <a
-              href={`https://twitter.com/${user.nickname}/status/${tweet.id_str}`}
-            ></a>
-          </blockquote>
-        ))}
-      </div>
+      <section className="flex flex-col items-center mx-auto">
+        <h2 className="pt-5 pb-4 pl-5 text-2xl font-bold leading-7 text-center text-gray-900 sm:text-3xl sm:truncate">
+          Congrats! 🎉 <p>Your tweet thread was successfully posted!</p>
+        </h2>
+        <span className="text-gray-500">
+          Click{" "}
+          <a
+            href={`https://twitter.com/${user.nickname}/status/${postedTweets[0].id_str}`}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-blue-500 hover:text-blue-600"
+          >
+            here
+          </a>{" "}
+          to view it on Twitter.
+        </span>
+        <div>
+          {postedTweets.map((tweet) => (
+            <blockquote class="twitter-tweet">
+              <a
+                href={`https://twitter.com/${user.nickname}/status/${tweet.id_str}`}
+              ></a>
+            </blockquote>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
